748d4fc5716f6e5629529554d023c1003bd5b894
Kishore Murari <Kishore.Murari@usps.gov>
2021-06-24 21:23:53 +0530

    Dynamic

--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class UspsServices:
+    id : int
+    serviceName : str    
--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,38 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import UspsServices
 
 def hello(request):
-   return render(request, 'bpgtemplate.html')
+   service1 = UspsServices()
+   service1.serviceName = "Freight Auction"
+   service1.serviceDescription = "Suppliers have the ability to accept or decline transportation based on a set rate."
+   service1.accessFlag = True
+   service1.id = 1
+
+   service2 = UspsServices()
+   service2.serviceName = "Contract Services"
+   service2.serviceDescription = "Suppliers have the ability to manage their contractual relationship with the US Postal Service including manifests (schedule), invoice and payment status."
+   service2.accessFlag = False
+   service2.id = 2
+
+   service3 = UspsServices()
+   service3.serviceName = "Transportation Procurement Services"
+   service3.serviceDescription = "Supplier has the ability to manage Transportation Procurement Services here"
+   service3.accessFlag = False
+   service3.id = 3
+
+   service4 = UspsServices()
+   service4.serviceName = "Automated Forms (Surface Transportation Automated Forms)"
+   service4.serviceDescription = "Supplier has the ability to manage . Automated Forms (Surface Transportation Automated Forms) here"
+   service4.accessFlag = False
+   service4.id = 4
+
+   service5 = UspsServices()
+   service5.serviceName = "Transportation Visibility & Performance"
+   service5.serviceDescription = "Supplier has the ability to manage Transportation Visibility &amp; Performance here... (refine description)."
+   service5.accessFlag = False
+   service5.id = 5
+
+   serviceList = [service1, service2, service3, service4, service5]
+ 
+   return render(request, 'bpgtemplate.html',{"serviceList":serviceList})
